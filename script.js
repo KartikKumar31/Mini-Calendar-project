93fb0c262fa7cfdf807d6280a6aca42454114669
Kartik Kumar <91678848+KartikKumar31@users.noreply.github.com>
2024-05-08 11:41:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const date = new Date()
 
 const weekDay= ["Sunday","Monday","Tuseday","Wednesday","Thursday","Friday","Saturday"]
-const monthName=["January","Feburary","March","April","May","June","July","August","September","October","November","December"]
+const monthName=["January","February","March","April","May","June","July","August","September","October","November","December"]
 
 document.getElementById('date').innerHTML=date.getDate()
 document.getElementById('day').innerHTML=weekDay[date.getDay()]
